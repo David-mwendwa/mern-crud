3a7dedadd4e957f022d262c9e755fe4f7a8aa4e0
techdave <klatzdavie19@gmail.com>
2023-03-10 08:54:05 +0300

    implement employee add functionality

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { EmployeeContext } from '../contexts/EmployeeContext';
+import useInput from './utils/useInput';
 
 const AddForm = () => {
+  const { addEmployee } = useContext(EmployeeContext);
+  const { values, resetValues, handleChange } = useInput({
+    name: '',
+    email: '',
+    address: '',
+    phone: '',
+  });
+  const { name, email, address, phone } = values;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEmployee({ name, email, address, phone });
+    resetValues();
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
-        <Form.Control type='text' placeholder='Name *' required></Form.Control>
+        <Form.Control
+          type='text'
+          placeholder='Name *'
+          name='name'
+          value={name}
+          onChange={handleChange}
+          required
+        />
       </Form.Group>
       <Form.Group>
         <Form.Control
-          type='eamil'
+          type='email'
           placeholder='Email *'
-          required></Form.Control>
+          name='email'
+          value={email}
+          onChange={handleChange}
+          required
+        />
       </Form.Group>
       <Form.Group>
         <Form.Control
           as='textarea'
           placeholder='Address'
-          rows={3}></Form.Control>
+          rows={3}
+          name='address'
+          value={address}
+          onChange={handleChange}
+        />
       </Form.Group>
       <Form.Group>
-        <Form.Control type='text' placeholder='Phone' required></Form.Control>
+        <Form.Control
+          type='text'
+          placeholder='Phone'
+          name='phone'
+          value={phone}
+          onChange={handleChange}
+        />
       </Form.Group>
       <Button variant='success' type='submit' block>
         Add New Employee
